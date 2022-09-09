3aabdc48d54db7876ed5bf1ed3207be7b76b146e
Alex Burneikis <alexburneikis@gmail.com>
2022-09-09 10:23:18 +1000

    Initial Commit

---
+++ MemojiScene/MemojiSceneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemojiSceneApp.swift
+//  MemojiScene
+//
+//  Created by Alexander Burneikis on 9/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemojiSceneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
