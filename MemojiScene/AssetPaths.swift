162f3cb4562914121eebc1c05161eabfac14d2f8
Alex Burneikis <alexburneikis@gmail.com>
2022-09-10 15:44:22 +1000

    Removed Animoji assets, added Memoji assets

---
+++ MemojiScene/AssetPaths.swift
@@ -0,0 +1,8 @@
+//
+//  AssetPaths.swift
+//  MemojiScene
+//
+//  Created by Alexander Burneikis on 10/9/2022.
+//
+
+import Foundation
--- MemojiScene/ContentView.swift
+++ MemojiScene/ContentView.swift
@@ -9,55 +9,37 @@ import SwiftUI
 import SceneKit
 import SceneView
 
-let alien = "alien.scnz"
-let dog = "dog.scnz"
+let head = "memoji/assets/faces/head.aa/main.scnz"
+let afro_long_down_sidePart = "memoji/assets/hair/hair_afro_long_down_sidePart.aa/main.scnz"
 
 let scene = SCNScene()
 let sceneView = SCNView()
 
 struct ContentView: View {
+    private var hairSelection: String = "afro_long_down_sidePart"
     var body: some View {
         VStack {
             SceneView(scene: scene, options: [.allowsCameraControl, .autoenablesDefaultLighting])
                 .frame(height: 300)
                 .onAppear {
-                    //add the alien scene into the scene
-                    let alienScene = SCNScene(named: alien)!
-                    let alienNodes = alienScene.rootNode.childNodes
-                    for node in alienNodes {
+                    //add the face scene into the scene
+                    let faceScene = SCNScene(named: head)!
+                    let faceNodes = faceScene.rootNode.childNodes
+                    for node in faceNodes {
                         scene.rootNode.addChildNode(node)
                     }
                 }
             List {
-                //make a button for dog and alien
-                Button(action: {
-                    //remove all nodes from the scene
-                    scene.rootNode.enumerateChildNodes { (node, _) in
-                        node.removeFromParentNode()
-                    }
-                    //add the dog scene into the scene
-                    let dogScene = SCNScene(named: dog)!
-                    let dogNodes = dogScene.rootNode.childNodes
-                    for node in dogNodes {
+                Button {
+                    let hairScene = SCNScene(named: afro_long_down_sidePart)!
+                    let hairNodes = hairScene.rootNode.childNodes
+                    for node in hairNodes {
                         scene.rootNode.addChildNode(node)
                     }
-                }, label: {
-                    Text("Dog")
-                })
-                Button(action: {
-                    //remove all nodes from the scene
-                    scene.rootNode.enumerateChildNodes { (node, _) in
-                        node.removeFromParentNode()
-                    }
-                    //add the alien scene into the scene
-                    let alienScene = SCNScene(named: alien)!
-                    let alienNodes = alienScene.rootNode.childNodes
-                    for node in alienNodes {
-                        scene.rootNode.addChildNode(node)
-                    }
-                }, label: {
-                    Text("Alien")
-                })
+                } label: {
+                    Text("Add Hair")
+                }
+
             }
         }
     }
